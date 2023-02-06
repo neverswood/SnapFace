38ecaa93e8e0b0e2771c377c2e08e70e17775124
neverswood <nevers_wood@hotmail.fr>
2023-02-06 15:31:13 +0100

    Create the service to the project

---
+++ src/app/services/face-snaps.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FaceSnapService {
+  faceSnaps: FaceSnap[] = [
+    {
+      id: 1,
+      title: 'yop',
+      description: 'Nos aventures!',
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: 'Paris',
+    },
+    {
+      id: 2,
+      title: 'Mathieu',
+      description: 'Nos meilleurs début',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+      createdDate: new Date(),
+      snaps: 250,
+      location: 'La montagne',
+    },
+    {
+      id: 3,
+      title: 'Léon',
+      description: 'Ses 18 ans',
+      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+    },
+  ];
+
+  getAllFaceSnaps(): FaceSnap[] {
+    return this.faceSnaps;
+  }
+
+  getFaceSnapById(faceSnapId: number): FaceSnap {
+    const faceSnap = this.faceSnaps.find(
+      (faceSnap) => faceSnap.id === faceSnapId
+    );
+    if (!faceSnap) {
+      throw new Error('FaceSnap not found!');
+    } else {
+      return faceSnap;
+    }
+  }
+
+  snapFacesnapById(faceSnapId: number, snapType: 'snap' | 'unsnap'): void {
+    const faceSnap = this.getFaceSnapById(faceSnapId);
+    snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
+  }
+}
